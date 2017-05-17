4e489d3b963a739ced15c03154cf7707881fba14
YhisLara <yhlara@alumnos.ubiobio.cl>
2017-05-17 12:43:24 -0400

    Create DataFragment.java

---
+++ DataFragment.java
@@ -0,0 +1,14 @@
+public class DataFragments extends Fragment{
+
+  public DataFragments(){
+  }
+  
+  public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+                             
+         View view = inflater.inflate(R.layout.fragment_data, container, false);
+         
+        // Inflate the layout for this fragment
+        return view
+    }
+}

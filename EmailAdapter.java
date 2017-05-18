c1f65a2b4b8edca5b7566124943b68806e714286
YhisLara <yhlara@alumnos.ubiobio.cl>
2017-05-18 12:23:33 -0400

    Update EmailAdapter.java

--- EmailAdapter.java
+++ EmailAdapter.java
@@ -1,11 +1,29 @@
 public class EmailAdapter extends BaseAdapter{
+   
+  private Context context;
+  private int layout;
+  private List<Email> mails;
+  
+  
+  public EmailAdapter(Context context , int layout, List<Email> mails){
+    this.context = context;
+    this.layout = layout;
+    this.mails = mails;
+  }
+  
+  public int getCount() {
+    return mails.size();
+  }
+  public Object getItem(int position) {
+    return mails.get(position);
+  }
+  
+  public long getItemId(int id) {
+        return id;
+  }
 
-public class EmailAdapter(){
-}
-
-public class EmailAdapter(){
-
-}
-
-
+  public int getItemCount() {
+        return 0;
+  }
+  
 }
